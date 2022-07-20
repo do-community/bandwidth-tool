7042407fce6119593aeb4833268a6b1b76619b4a
Matt Cowley <me@mattcowley.co.uk>
2022-07-20 21:34:00 +0100

    Dependency updates (#68)

--- src/build/get.js
+++ src/build/get.js
@@ -1,6 +1,23 @@
-const fetch = require('node-fetch');
+/*
+Copyright 2022 DigitalOcean
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 const fs = require('fs');
 const path = require('path');
+
+const fetch = import('node-fetch');
 
 let token = process.env.DIGITALOCEAN_TOKEN;
 if (fs.existsSync(path.join(__dirname, '..', '..', 'config.js'))) {
@@ -9,8 +26,8 @@ if (fs.existsSync(path.join(__dirname, '..', '..', 'config.js'))) {
 }
 
 module.exports = async url => {
-    const res = await fetch(url, {
+    const res = await fetch.then(({ default: run }) => run(url, {
         headers: { Authorization: `Bearer ${token}` },
-    });
+    }));
     return await res.json();
 };
